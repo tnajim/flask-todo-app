6b4512c7a204b3d0cdc8a924d8a943e8ae910ace
Taher Najim <taher.najim@yahoo.com>
2024-06-13 12:26:30 +0800

    added update route

--- app.py
+++ app.py
@@ -41,6 +41,20 @@ def delete(id):
     return redirect('/')
   except:
     return 'There was a problem deleting that task'
+  
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+  task = Todo.query.get_or_404(id)
+
+  if request.method == 'POST':
+    task.content = request.form['content']
+    try:
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was a problem updating the task'
+  else:
+    return render_template('update.html', task=task)
 
 if __name__ == "__main__":
   app.run(debug=True, port=5001)
